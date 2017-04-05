67eeadd76c43f51fc6a7733fced7c991fbb18e38
Blake Embrey <hello@blakeembrey.com>
2017-04-05 07:56:06 -0700

    Update return value handler

--- src/index.ts
+++ src/index.ts
@@ -12,20 +12,26 @@ export interface ErrorHandler <T, U> {
   (err: Error, req: T, res: U, next: NextFunction): any
 }
 
+function handle (result: any, next: NextFunction) {
+  if (result && typeof result.then === 'function') {
+    return result.then(undefined, function (err: Error) {
+      return next(err || new Error('Promise was rejected with a falsy value'))
+    })
+  }
+
+  return result
+}
+
 export function wrap <T, U> (fn: Handler<T, U>): Handler<T, U>
 export function wrap <T, U> (fn: ErrorHandler<T, U>): ErrorHandler<T, U>
 export function wrap <T, U> (fn: Handler<T, U> | ErrorHandler<T, U>): Handler<T, U> | ErrorHandler<T, U> {
   if (fn.length === 4) {
     return function (err: Error, req: T, res: U, next: NextFunction): any {
-      const result = (fn as ErrorHandler<T, U>)(err, req, res, next)
-
-      return Promise.resolve(result).catch(err => next(err))
+      return handle((fn as ErrorHandler<T, U>)(err, req, res, next), next)
     }
   }
 
   return function (req: T, res: U, next: NextFunction): any {
-    const result = (fn as Handler<T, U>)(req, res, next)
-
-    return Promise.resolve(result).catch(err => next(err))
+    return handle((fn as Handler<T, U>)(req, res, next), next)
   }
 }
